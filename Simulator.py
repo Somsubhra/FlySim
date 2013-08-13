c72666a21f182a0f0b463f4ad576ebdad8c29ead
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2013-08-14 00:22:56 +0530

    Added a plane. A Boeing 707

--- Simulator.py
+++ Simulator.py
@@ -1,6 +1,7 @@
 from math import pi, sin, cos
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
+from direct.actor.Actor import Actor
 
 class FliSim(ShowBase):
 
@@ -8,6 +9,12 @@ class FliSim(ShowBase):
 	def __init__(self):
 		ShowBase.__init__(self)
 		self.setBackgroundColor(0, 0, 0)
+
+		self.plane = self.loader.loadModel("./models/plane/boeing707")
+		self.plane.setScale(0.07, 0.07, 0.07)
+		self.plane.setPos(0, 7, 3.0)
+		self.plane.reparentTo(self.render)
+
 		self.environ = self.loader.loadModel("./models/sky/skysphere")
 		self.i = -16
 		self.environ.reparentTo(self.render)
@@ -18,11 +25,12 @@ class FliSim(ShowBase):
 		self.taskMgr.add(self.cameraTask, "cameraTask")
 
 	def cameraTask(self, task):
-		if(str(self.i) == str(1.0)):
+		if(str(self.i) == str(-4.0)):
 			self.i = -16
 
 		self.i = self.i+0.1
 		self.camera.setPos(0, self.i, 5)
+		self.plane.setPos(0, self.i+12, 3.0)
 		print(str(self.i))
 		return Task.cont
 
