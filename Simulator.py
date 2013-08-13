1e8157a1e79d88a5e1e0277a7b91f56cf4a06adf
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2013-08-13 23:58:21 +0530

    A successful one

--- Simulator.py
+++ Simulator.py
@@ -1,25 +1,29 @@
 from math import pi, sin, cos
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
+
 class FliSim(ShowBase):
 
+	#self.i = 0
 	def __init__(self):
 		ShowBase.__init__(self)
 		self.setBackgroundColor(0, 0, 0)
-		self.environ = self.loader.loadModel("./models/forestsky")
-
+		self.environ = self.loader.loadModel("./models/sky/cloudysky")
+		self.i = -16
 		self.environ.reparentTo(self.render)
 
-		self.environ.setScale(0.50, 0.50, 0.50)
-		self.environ.setPos(-15 , -15, 20)
+		self.environ.setScale(0.038, 0.038, 0.038)
+		self.environ.setPos(0, 0, 0)
 
-		self.taskMgr.add(self.spinCameraTask, "spinCameraTask")
+		self.taskMgr.add(self.cameraTask, "cameraTask")
 
-	def spinCameraTask(self, task):
-		angleDegrees = task.time * 30.0
-		angleRadians = angleDegrees * (pi / 180.0)
-		self.camera.setPos(20 * sin(angleRadians), -20.0 * cos(angleRadians), 3)
-		self.camera.setHpr(angleDegrees, 0, 0)
+	def cameraTask(self, task):
+		if(str(self.i) == str(1.0)):
+			self.i = -16
+
+		self.i = self.i+0.1
+		self.camera.setPos(0, self.i, 5)
+		print(str(self.i))
 		return Task.cont
 
 sim = FliSim()
