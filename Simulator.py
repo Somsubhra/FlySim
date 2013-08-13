beda08ccf7cfdb18711be395737afbb16c96447a
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2013-08-13 12:59:40 +0530

    Make the scene perfect

--- Simulator.py
+++ Simulator.py
@@ -5,18 +5,18 @@ class FliSim(ShowBase):
 
 	def __init__(self):
 		ShowBase.__init__(self)
-
+		self.setBackgroundColor(0, 0, 0)
 		self.environ = self.loader.loadModel("./models/forestsky")
 
 		self.environ.reparentTo(self.render)
 
 		self.environ.setScale(0.50, 0.50, 0.50)
-		self.environ.setPos(-15 , -20, 17)
+		self.environ.setPos(-15 , -15, 20)
 
 		self.taskMgr.add(self.spinCameraTask, "spinCameraTask")
 
 	def spinCameraTask(self, task):
-		angleDegrees = task.time * 6.0
+		angleDegrees = task.time * 30.0
 		angleRadians = angleDegrees * (pi / 180.0)
 		self.camera.setPos(20 * sin(angleRadians), -20.0 * cos(angleRadians), 3)
 		self.camera.setHpr(angleDegrees, 0, 0)
