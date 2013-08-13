88fed282224131dcec1fb56652cd64da7394f446
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2013-08-13 12:49:25 +0530

    Added an initital scene

---
+++ Simulator.py
@@ -0,0 +1,26 @@
+from math import pi, sin, cos
+from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
+class FliSim(ShowBase):
+
+	def __init__(self):
+		ShowBase.__init__(self)
+
+		self.environ = self.loader.loadModel("./models/forestsky")
+
+		self.environ.reparentTo(self.render)
+
+		self.environ.setScale(0.50, 0.50, 0.50)
+		self.environ.setPos(-15 , -20, 17)
+
+		self.taskMgr.add(self.spinCameraTask, "spinCameraTask")
+
+	def spinCameraTask(self, task):
+		angleDegrees = task.time * 6.0
+		angleRadians = angleDegrees * (pi / 180.0)
+		self.camera.setPos(20 * sin(angleRadians), -20.0 * cos(angleRadians), 3)
+		self.camera.setHpr(angleDegrees, 0, 0)
+		return Task.cont
+
+sim = FliSim()
+sim.run()
