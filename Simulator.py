a315ace6317a1664c0af4f2dd884f5101a002f31
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2013-08-14 16:33:07 +0530

    Remove previous Simulator

--- Simulator.py
+++ Simulator.py
@@ -1,38 +1,74 @@
-from math import pi, sin, cos
-from direct.showbase.ShowBase import ShowBase
-from direct.task import Task
-from direct.actor.Actor import Actor
+import direct.directbase.DirectStart 
+from panda3d.core import Fog
+from direct.showbase.DirectObject import DirectObject
+from direct.interval.MetaInterval import Sequence
+from direct.interval.LerpInterval import LerpFunc
+from direct.interval.FunctionInterval import Func
+import sys
 
-class FliSim(ShowBase):
+TUNNEL_SEGMENT_LENGTH = 50           
+TUNNEL_TIME = 2
 
-	#self.i = 0
-	def __init__(self):
-		ShowBase.__init__(self)
-		self.setBackgroundColor(0, 0, 255)
+class World(DirectObject):
+  
+  def __init__(self):
 
-		self.plane = self.loader.loadModel("./models/plane/boeing707")
-		self.plane.setScale(0.07, 0.07, 0.07)
-		self.plane.setPos(0, 7, 1.5)
-		self.plane.reparentTo(self.render)
+  	self.plane = loader.loadModel('./models/plane/boeing707')
+  	self.plane.reparentTo(render)
+  	self.plane.setScale(0.04, 0.04, 0.04)
+  	self.plane.setPos(0, 10, 0)
+  	self.plane.setPosHpr(0,-0.7,0,0,270,0)
 
-		self.environ = self.loader.loadModel("./models/sky/skysphere")
-		self.i = -16
-		self.environ.reparentTo(self.render)
+  	base.disableMouse()
+   	camera.setPosHpr(0,0.5,10, 0, -100, 0) #Vary this
+   	base.setBackgroundColor(0,0.5,1)
+    
+   	self.fog = Fog('distanceFog')
 
-		self.environ.setScale(0.038, 0.038, 0.038)
-		self.environ.setPos(0, 0, 0)
+   	self.fog.setColor(0, 0.5, 1)
 
-		self.taskMgr.add(self.cameraTask, "cameraTask")
+   	self.fog.setExpDensity(.08)
 
-	def cameraTask(self, task):
-		if(str(self.i) == str(-4.0)):
-			self.i = -16
+   	render.setFog(self.fog)
 
-		self.i = self.i+0.1
-		self.camera.setPos(0, self.i, 3.5)
-		self.plane.setPos(0, self.i+12, 1.5)
-		print(str(self.i))
-		return Task.cont
+   	self.initTunnel()
+   	self.contTunnel()
 
-sim = FliSim()
-sim.run()
+    
+  def initTunnel(self):
+    self.tunnel = [None for i in range(4)]
+    
+    for x in range(4):
+      self.tunnel[x] = loader.loadModel('models/terrain/tunnel')
+
+      if x == 0: self.tunnel[x].reparentTo(render)
+
+      else:      self.tunnel[x].reparentTo(self.tunnel[x-1])
+
+      self.tunnel[x].setPos(0, 0, -TUNNEL_SEGMENT_LENGTH)
+
+  def contTunnel(self):
+    self.tunnel = self.tunnel[1:]+ self.tunnel[0:1]
+
+    self.tunnel[0].setZ(0)
+
+    self.tunnel[0].reparentTo(render)
+
+    self.tunnel[0].setScale(.155, .155, .305)
+
+    self.tunnel[3].reparentTo(self.tunnel[2])
+    self.tunnel[3].setZ(-TUNNEL_SEGMENT_LENGTH)
+    self.tunnel[3].setScale(1)
+
+    self.tunnelMove = Sequence(
+      LerpFunc(self.tunnel[0].setZ,
+               duration = TUNNEL_TIME,
+               fromData = 0,
+               toData = TUNNEL_SEGMENT_LENGTH*.305),
+      Func(self.contTunnel)
+      )
+    self.tunnelMove.start()
+
+w = World()
+run()
+
