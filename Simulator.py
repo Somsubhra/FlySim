29aa6c3764a5ee7d9ed510c7ad68276bc32bef03
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2013-08-14 16:48:42 +0530

    Added left right finctionalitites

--- Simulator.py
+++ Simulator.py
@@ -10,14 +10,15 @@ TUNNEL_SEGMENT_LENGTH = 50
 TUNNEL_TIME = 2
 
 class World(DirectObject):
-  
+
   def __init__(self):
 
   	self.plane = loader.loadModel('./models/plane/boeing707')
   	self.plane.reparentTo(render)
   	self.plane.setScale(0.04, 0.04, 0.04)
-  	self.plane.setPos(0, 10, 0)
-  	self.plane.setPosHpr(0,-0.7,0,0,270,0)
+  	
+  	self.xPos = 0
+  	self.plane.setPosHpr(self.xPos,-0.7,0,0,270,0)
 
   	base.disableMouse()
    	camera.setPosHpr(0,0.5,10, 0, -100, 0) #Vary this
@@ -34,7 +35,19 @@ class World(DirectObject):
    	self.initTunnel()
    	self.contTunnel()
 
+   	self.accept('escape', sys.exit)
+   	self.accept('d', self.moveRight)
+   	self.accept('a', self.moveLeft)
     
+  def moveRight(self):
+  	self.xPos = self.xPos + 0.1
+  	self.plane.setPosHpr(self.xPos, -0.7, 0, 0, 270, 0)
+
+
+  def moveLeft(self):
+  	self.xPos = self.xPos - 0.1
+  	self.plane.setPosHpr(self.xPos, -0.7, 0, 0, 270, 0)
+  
   def initTunnel(self):
     self.tunnel = [None for i in range(4)]
     
