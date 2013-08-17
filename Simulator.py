002f3fc060dc67591281f95bdd1fad4297e6f22f
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2013-08-17 20:54:46 +0530

    Fix every damn indentation

--- Simulator.py
+++ Simulator.py
@@ -17,128 +17,128 @@ TUNNEL_TIME = 15
 class World(DirectObject):
 
 # Initialize the world class
-  def __init__(self):
+	def __init__(self):
 
 # Load the Boeing707
-    self.scale = 0.04
-    self.plane = loader.loadModel('./models/plane/boeing707')
-    self.plane.reparentTo(render)
-    self.plane.setScale(self.scale, self.scale, self.scale)  	
-    self.xPos = 0
-    self.plane.setPosHpr(self.xPos, -0.7, 0, 0, 270, 0)
+		self.scale = 0.04
+		self.plane = loader.loadModel('./models/plane/boeing707')
+		self.plane.reparentTo(render)
+		self.plane.setScale(self.scale, self.scale, self.scale)  	
+		self.xPos = 0
+		self.plane.setPosHpr(self.xPos, -0.7, 0, 0, 270, 0)
 
 
-    base.disableMouse()
-    camera.setPosHpr(0, 0.5, 10, 0, -100, 0) #Vary this
-    base.setBackgroundColor(0, 0.5, 1)
+		base.disableMouse()
+		camera.setPosHpr(0, 0.5, 10, 0, -100, 0) #Vary this
+		base.setBackgroundColor(0, 0.5, 1)
     
 # Load fog into the view
-    self.fog = Fog('distanceFog')
-    self.fog.setColor(0, 0.5, 1)
-    self.fog.setExpDensity(.08)
-    render.setFog(self.fog)
+		self.fog = Fog('distanceFog')
+		self.fog.setColor(0, 0.5, 1)
+		self.fog.setExpDensity(.08)
+		render.setFog(self.fog)
 
 # Load the tunnel and keep it running infinitely
-    self.initTunnel()
-    self.contTunnel()
+		self.initTunnel()
+		self.contTunnel()
 
 # Key mappings
-    self.accept('escape', sys.exit)
-    self.accept('d', self.moveRight)
-    self.accept('a', self.moveLeft)
-    self.accept('+', self.scaleUp)
-    self.accept('-', self.scaleDown)
+		self.accept('escape', sys.exit)
+		self.accept('d', self.moveRight)
+		self.accept('a', self.moveLeft)
+		self.accept('+', self.scaleUp)
+		self.accept('-', self.scaleDown)
 
-    try:
-      self.ser = serial.Serial('/dev/ttyUSB3', 9600)
-      taskMgr.add(self.serialTask, "serialTask")
-    except:
-      print("Could not open Serial port")
+		try:
+			self.ser = serial.Serial('/dev/ttyUSB3', 9600)
+			taskMgr.add(self.serialTask, "serialTask")
+		except:
+			print("Could not open Serial port")
 
 # The serial task
-  def serialTask(self, task):
-    reading = self.ser.readline()
-    print reading
-    if(reading != ""):
-        try:
-		x = float(reading)
-		dev = (320.0 - x)
-		print "dev:"
-		print dev
+	def serialTask(self, task):
+		reading = self.ser.readline()
+		print reading
+		if(reading != ""):
+			try:
+				x = float(reading)
+				dev = (320.0 - x)
+				print "dev:"
+				print dev
 		
-		if(dev < -30):
-			self.moveLeft()
-#			self.xPos = self.xPos - 0.1
-#			self.plane.setPosHpr(self.xPos, -0.7, 0, 0, 270, 0)
-		elif(dev > 30):
-			self.moveRight()
-#			self.xPos = self.xPos + 0.1
-#			self.plane.setPosHpr(self)
-#		self.plane.setPosHpr(self.xPos + dev, -0.7, 0, 0, 270, 0)
-		else:
-		    pass
-        except:
-    	    print "Couldn't parse"
+				if(dev < -30):
+					self.moveLeft()
+#					self.xPos = self.xPos - 0.1
+#					self.plane.setPosHpr(self.xPos, -0.7, 0, 0, 270, 0)
+				elif(dev > 30):
+					self.moveRight()
+#					self.xPos = self.xPos + 0.1
+#					self.plane.setPosHpr(self)
+#				self.plane.setPosHpr(self.xPos + dev, -0.7, 0, 0, 270, 0)
+				else:
+					pass
+			except:
+				print "Couldn't parse"
 
-    return Task.cont
+		return Task.cont
 
 # Zoom into the plane
-  def scaleUp(self):
-    self.scale = self.scale + 0.005
-    self.plane.setScale(self.scale, self.scale, self.scale)
+	def scaleUp(self):
+		self.scale = self.scale + 0.005
+		self.plane.setScale(self.scale, self.scale, self.scale)
 
 # Zoom out of the plane
-  def scaleDown(self):
-    self.scale = self.scale - 0.005
-    self.plane.setScale(self.scale, self.scale, self.scale)
+	def scaleDown(self):
+		self.scale = self.scale - 0.005
+		self.plane.setScale(self.scale, self.scale, self.scale)
   	
 # Move the plane right
-  def moveRight(self):
-    self.xPos = self.xPos + 0.01
-    self.plane.setPosHpr(self.xPos, -0.7, 0, 0, 270, 0)
+	def moveRight(self):
+		self.xPos = self.xPos + 0.01
+		self.plane.setPosHpr(self.xPos, -0.7, 0, 0, 270, 0)
 
 # Move the plane left
-  def moveLeft(self):
-    self.xPos = self.xPos - 0.01
-    self.plane.setPosHpr(self.xPos, -0.7, 0, 0, 270, 0)
+	def moveLeft(self):
+		self.xPos = self.xPos - 0.01
+		self.plane.setPosHpr(self.xPos, -0.7, 0, 0, 270, 0)
   
 # The tunnel initialization function
-  def initTunnel(self):
-    self.tunnel = [None for i in range(4)]
+	def initTunnel(self):
+		self.tunnel = [None for i in range(4)]
     
-    for x in range(4):
-      self.tunnel[x] = loader.loadModel('models/terrain/tunnel')
+		for x in range(4):
+			self.tunnel[x] = loader.loadModel('models/terrain/tunnel')
 
-      if x == 0:
-	self.tunnel[x].reparentTo(render)
+			if x == 0:
+				self.tunnel[x].reparentTo(render)
 
-      else:
-	self.tunnel[x].reparentTo(self.tunnel[x-1])
+			else:
+				self.tunnel[x].reparentTo(self.tunnel[x-1])
 
-      self.tunnel[x].setPos(0, 0, -TUNNEL_SEGMENT_LENGTH)
+			self.tunnel[x].setPos(0, 0, -TUNNEL_SEGMENT_LENGTH)
 
 # Function to keep the tunnel running infinite
-  def contTunnel(self):
-    self.tunnel = self.tunnel[1:]+ self.tunnel[0:1]
+	def contTunnel(self):
+		self.tunnel = self.tunnel[1:]+ self.tunnel[0:1]
 
-    self.tunnel[0].setZ(0)
+		self.tunnel[0].setZ(0)
 
-    self.tunnel[0].reparentTo(render)
+		self.tunnel[0].reparentTo(render)
 
-    self.tunnel[0].setScale(.355, .355, .505)
+		self.tunnel[0].setScale(.355, .355, .505)
 
-    self.tunnel[3].reparentTo(self.tunnel[2])
-    self.tunnel[3].setZ(-TUNNEL_SEGMENT_LENGTH)
-    self.tunnel[3].setScale(1)
+		self.tunnel[3].reparentTo(self.tunnel[2])
+		self.tunnel[3].setZ(-TUNNEL_SEGMENT_LENGTH)
+		self.tunnel[3].setScale(1)
 
-    self.tunnelMove = Sequence(
-      LerpFunc(self.tunnel[0].setZ,
-               duration = TUNNEL_TIME,
-               fromData = 0,
-               toData = TUNNEL_SEGMENT_LENGTH*.305),
-      Func(self.contTunnel)
-      )
-    self.tunnelMove.start()
+		self.tunnelMove = Sequence(
+			LerpFunc(self.tunnel[0].setZ,
+					duration = TUNNEL_TIME,
+					fromData = 0,
+					toData = TUNNEL_SEGMENT_LENGTH*.305),
+			Func(self.contTunnel)
+		)
+		self.tunnelMove.start()
 
 # Load the world
 w = World()
